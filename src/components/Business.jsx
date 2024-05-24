bc3757eee9188e1e2ad2f70365ff6a67a537689a
Yidne <Yamiyekn@gmail.com>
2024-05-24 09:44:11 +0300

    Add Business section.

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { features } from "../constants";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
-const Business = () => {
-	return <div></div>;
-};
+const Business = () => (
+	<section id="features" className={layout.section}>
+		<div className={layout.sectionInfo}>
+			<h2 className={styles.heading2}>
+				You do the business, <br className="sm:block hidden" /> we’ll handle the
+				money.
+			</h2>
+			<p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+				With the right credit card, you can improve your financial life by
+				building credit, earning rewards and saving money. But with hundreds of
+				credit cards on the market.
+			</p>
+
+			<Button styles={`mt-10`} />
+		</div>
+
+		<div className={`${layout.sectionImg} flex-col`}>
+			{features.map((feature, index) => (
+				<FeatureCard key={feature.id} {...feature} index={index} />
+			))}
+		</div>
+	</section>
+);
 
 export default Business;
