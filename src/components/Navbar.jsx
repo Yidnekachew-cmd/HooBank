36d4bde03480d38344fb729840455a32ff7180eb
Yidne <Yamiyekn@gmail.com>
2024-05-24 09:29:11 +0300

    Create a navbar for the mobile.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,6 +23,32 @@ const Navbar = () => {
 					</li>
 				))}
 			</ul>
+			<div className="sm:hidden flex flex-1 justify-end items-center">
+				<img
+					src={toggle ? close : menu}
+					alt="menu"
+					className="w-[28px] h-[28px] object-contain"
+					onClick={() => setToggle(!toggle)}
+				/>
+
+				<div
+					className={`${
+						!toggle ? "hidden" : "flex"
+					} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
+					<ul className="list-none flex justify-end items-start flex-1 flex-col">
+						{navLinks.map((nav, index) => (
+							<li
+								key={nav.id}
+								className={`font-poppins font-medium cursor-pointer text-[16px] ${
+									active === nav.title ? "text-white" : "text-dimWhite"
+								} ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
+								onClick={() => setActive(nav.title)}>
+								<a href={`#${nav.id}`}>{nav.title}</a>
+							</li>
+						))}
+					</ul>
+				</div>
+			</div>
 		</nav>
 	);
 };
