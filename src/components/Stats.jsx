393c1ee15587ddad4dad37c2d6b60fda43dadff4
Yidne <Yamiyekn@gmail.com>
2024-05-24 09:40:45 +0300

    Add stats section.

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import { stats } from "../constants";
+import styles from "../style";
 
-const Stats = () => {
-	return <div></div>;
-};
+const Stats = () => (
+	<section
+		className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}></section>
+);
 
 export default Stats;
